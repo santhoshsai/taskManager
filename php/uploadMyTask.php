372528bbc582d9510cb6625648c1faee64041d39
saisanthosh.saikumar <saisanthosh.saikumar@snmg.Impigertech.com>
2016-11-09 12:13:02 +0530

    task manager base file for CURD of a task

---
+++ php/uploadMyTask.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * This file will upload user entered details
+ * with all CURD operation
+ * 
+ */
+
+class uploadMyTask{
+    
+    function createNewTask()
+    {
+        
+    }
+    
+    function updateExistingTask()
+    {
+        
+    }
+    
+}
